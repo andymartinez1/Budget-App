31ead4f4bed8ca7b9ada0960ba5f6b8420742021
Andrew Martinez <martinez.s.andrew@gmail.com>
2026-01-02 17:20:17 -0500

    Created an account controller and login page. Working on adding a register page.

---
+++ BudgetApp/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BudgetApp.Controllers;
+
+public class AccountController : Controller
+{
+    // GET
+    public IActionResult Login()
+    {
+        return View();
+    }
+}
