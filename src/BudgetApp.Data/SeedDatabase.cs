dc85d1506e3b97101a7ed6e91a62522544065ca4
Andrew Martinez <martinez.s.andrew@gmail.com>
2026-04-15 20:02:44 -0400

    Made changes to data seeding to prevent seeding if users already exist. Removed redundant database ensure deleted call.

--- src/BudgetApp.Data/SeedDatabase.cs
+++ src/BudgetApp.Data/SeedDatabase.cs
@@ -15,6 +15,13 @@ public static class SeedDatabase
             )
         )
         {
+            context.Database.EnsureCreated();
+
+            if (context.Users.Any())
+            {
+                return;
+            }
+
             var hasher = new PasswordHasher<BudgetUser>();
             context.Users.Add(
                 new BudgetUser
@@ -231,8 +238,6 @@ public static class SeedDatabase
                 }
             );
 
-            context.Database.EnsureDeleted();
-            context.Database.EnsureCreated();
             context.SaveChanges();
         }
     }
