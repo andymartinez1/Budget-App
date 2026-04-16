6af2146f7b3c289d5f9503eb32b65223b442a38e
Andrew Martinez <martinez.s.andrew@gmail.com>
2026-04-16 10:03:46 -0400

    Added unit tests for Category and Transaction controllers, covering index, create, edit, and delete actions with appropriate assertions.

--- tests/BudgetApp.UnitTests/CategoryControllerTests.cs
+++ tests/BudgetApp.UnitTests/CategoryControllerTests.cs
@@ -1,4 +1,5 @@
 ﻿using BudgetApp.Entities;
+using BudgetApp.Models;
 using BudgetApp.UnitTests.Helpers;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
@@ -9,12 +10,95 @@ namespace BudgetApp.UnitTests;
 public class CategoryControllerTests
 {
     [Fact]
-    public async Task Edit_Post_UpdatesCategoryAndCallsUpdateService()
+    public async Task Index_ReturnsViewWithViewModel()
+    {
+        // Arrange
+        var (controller, catMock, _) = UnitTestsHelper.CreateCategoryControllerWithMocks();
+        var categories = new List<Category> { UnitTestsHelper.GetTestCategory() };
+        catMock.Setup(s => s.GetAllAsync()).ReturnsAsync(categories);
+
+        // Act
+        var result = await controller.Index();
+
+        // Assert
+        var viewResult = Assert.IsType<ViewResult>(result);
+        var model = Assert.IsType<TransactionCategoryViewModel>(viewResult.Model);
+        Assert.Single(model.Categories);
+    }
+
+    [Fact]
+    public async Task Create_Get_ReturnsPartialView()
+    {
+        // Arrange
+        var (controller, _, _) = UnitTestsHelper.CreateCategoryControllerWithMocks();
+
+        // Act
+        var result = await controller.Create();
+
+        // Assert
+        var partialViewResult = Assert.IsType<PartialViewResult>(result);
+        Assert.Equal("_CreateCategoryModalPartial", partialViewResult.ViewName);
+        Assert.IsType<CategoryViewModel>(partialViewResult.Model);
+    }
+
+    [Fact]
+    public async Task Create_Post_ValidModel_RedirectsToIndex()
+    {
+        // Arrange
+        var (controller, catMock, _) = UnitTestsHelper.CreateCategoryControllerWithMocks();
+        var categoryVm = new CategoryViewModel { Name = "New Category" };
+
+        // Act
+        var result = await controller.Create(categoryVm);
+
+        // Assert
+        var redirectResult = Assert.IsType<RedirectToActionResult>(result);
+        Assert.Equal("Index", redirectResult.ActionName);
+        catMock.Verify(s => s.AddAsync(categoryVm), Times.Once);
+    }
+
+    [Fact]
+    public async Task Create_Post_InvalidModel_ReturnsPartialView()
+    {
+        // Arrange
+        var (controller, catMock, _) = UnitTestsHelper.CreateCategoryControllerWithMocks();
+        var categoryVm = new CategoryViewModel();
+        controller.ModelState.AddModelError("Name", "Required");
+
+        // Act
+        var result = await controller.Create(categoryVm);
+
+        // Assert
+        var partialViewResult = Assert.IsType<PartialViewResult>(result);
+        Assert.Equal("_CreateCategoryModalPartial", partialViewResult.ViewName);
+        catMock.Verify(s => s.AddAsync(It.IsAny<CategoryViewModel>()), Times.Never);
+    }
+
+    [Fact]
+    public async Task Edit_Get_ReturnsPartialView()
     {
         // Arrange
         var (controller, catMock, _) = UnitTestsHelper.CreateCategoryControllerWithMocks();
         var category = UnitTestsHelper.GetTestCategory();
-        var categoryVm = UnitTestsHelper.GetCategoryViewModel();
+        catMock.Setup(s => s.GetByIdAsync(category.Id)).ReturnsAsync(category);
+
+        // Act
+        var result = await controller.Edit(category.Id);
+
+        // Assert
+        var partialViewResult = Assert.IsType<PartialViewResult>(result);
+        Assert.Equal("_EditCategoryModalPartial", partialViewResult.ViewName);
+        var model = Assert.IsType<CategoryViewModel>(partialViewResult.Model);
+        Assert.Equal(category.Id, model.CategoryId);
+    }
+
+    [Fact]
+    public async Task Edit_Post_ValidModel_UpdatesAndReturnsCreatedAtAction()
+    {
+        // Arrange
+        var (controller, catMock, _) = UnitTestsHelper.CreateCategoryControllerWithMocks();
+        var category = UnitTestsHelper.GetTestCategory();
+        var categoryVm = new CategoryViewModel { CategoryId = category.Id, Name = "Updated Name" };
 
         catMock.Setup(s => s.GetByIdAsync(category.Id)).ReturnsAsync(category);
 
@@ -22,9 +106,64 @@ public class CategoryControllerTests
         var result = await controller.Edit(category.Id, categoryVm);
 
         // Assert
-        var ok = Assert.IsType<CreatedAtActionResult>(result);
-        var returned = Assert.IsType<Category>(ok.Value);
-        Assert.Equal(categoryVm.Name, returned.Name);
-        Mock.Get(catMock.Object).Verify(s => s.UpdateAsync(category.Id), Times.Once);
+        var createdResult = Assert.IsType<CreatedAtActionResult>(result);
+        Assert.Equal("Index", createdResult.ActionName);
+        var returned = Assert.IsType<Category>(createdResult.Value);
+        Assert.Equal("Updated Name", returned.Name);
+        catMock.Verify(s => s.UpdateAsync(category.Id, categoryVm), Times.Once);
+    }
+
+    [Fact]
+    public async Task Edit_Post_InvalidModel_ReturnsPartialView()
+    {
+        // Arrange
+        var (controller, catMock, _) = UnitTestsHelper.CreateCategoryControllerWithMocks();
+        var categoryVm = new CategoryViewModel();
+        controller.ModelState.AddModelError("Name", "Required");
+
+        // Act
+        var result = await controller.Edit(1, categoryVm);
+
+        // Assert
+        var partialViewResult = Assert.IsType<PartialViewResult>(result);
+        Assert.Equal("_EditCategoryModalPartial", partialViewResult.ViewName);
+        catMock.Verify(
+            s => s.UpdateAsync(It.IsAny<int>(), It.IsAny<CategoryViewModel>()),
+            Times.Never
+        );
+    }
+
+    [Fact]
+    public async Task Delete_Get_ReturnsPartialView()
+    {
+        // Arrange
+        var (controller, catMock, _) = UnitTestsHelper.CreateCategoryControllerWithMocks();
+        var category = UnitTestsHelper.GetTestCategory();
+        catMock.Setup(s => s.GetByIdAsync(category.Id)).ReturnsAsync(category);
+
+        // Act
+        var result = await controller.Delete(category.Id);
+
+        // Assert
+        var partialViewResult = Assert.IsType<PartialViewResult>(result);
+        Assert.Equal("_DeleteCategoryModalPartial", partialViewResult.ViewName);
+        var model = Assert.IsType<CategoryViewModel>(partialViewResult.Model);
+        Assert.Equal(category.Name, model.Name);
+    }
+
+    [Fact]
+    public async Task DeleteConfirmed_Post_RedirectsToIndex()
+    {
+        // Arrange
+        var (controller, catMock, _) = UnitTestsHelper.CreateCategoryControllerWithMocks();
+        int categoryId = 42;
+
+        // Act
+        var result = await controller.DeleteConfirmed(categoryId);
+
+        // Assert
+        var redirectResult = Assert.IsType<RedirectToActionResult>(result);
+        Assert.Equal("Index", redirectResult.ActionName);
+        catMock.Verify(s => s.DeleteAsync(categoryId), Times.Once);
     }
 }
--- tests/BudgetApp.UnitTests/TransactionControllerTests.cs
+++ tests/BudgetApp.UnitTests/TransactionControllerTests.cs
@@ -1,7 +1,6 @@
 ﻿using BudgetApp.Controllers;
 using BudgetApp.Entities;
 using BudgetApp.Models;
-using BudgetApp.ServiceContracts.DTO;
 using BudgetApp.UnitTests.Helpers;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
@@ -12,61 +11,178 @@ namespace BudgetApp.UnitTests;
 public class TransactionControllerTests
 {
     [Fact]
-    public async Task Details_ReturnsViewWithTransaction_WhenTransactionExists()
+    public async Task Index_ReturnsViewWithViewModel()
     {
         // Arrange
         var (controller, txMock, catMock, _) =
             UnitTestsHelper.CreateTransactionControllerWithMocks();
-        var category = UnitTestsHelper.GetTestCategory();
-        var transaction = UnitTestsHelper.GetTestTransaction();
-        var transactionVm = UnitTestsHelper.GetTransactionViewModel();
+        var transactions = new List<Transaction> { UnitTestsHelper.GetTestTransaction() };
+        var categories = new List<Category> { UnitTestsHelper.GetTestCategory() };
 
-        txMock.Setup(s => s.GetByIdAsync(transaction.Id)).ReturnsAsync(transaction);
-        catMock.Setup(c => c.GetByIdAsync(transaction.CategoryId)).ReturnsAsync(category);
+        txMock.Setup(s => s.GetAllAsync()).ReturnsAsync(transactions);
+        catMock.Setup(s => s.GetAllAsync()).ReturnsAsync(categories);
 
         // Act
-        var result = await controller.Details(transactionVm.TransactionId);
+        var result = await controller.Index(null, null, null, null);
 
         // Assert
-        var viewResult = Assert.IsType<PartialViewResult>(result);
-        Assert.IsType<TransactionViewModel>(viewResult.Model);
+        var viewResult = Assert.IsType<ViewResult>(result);
+        var model = Assert.IsType<TransactionCategoryViewModel>(viewResult.Model);
+        Assert.Single(model.Transactions);
+        Assert.NotEmpty(model.CategoriesSelectList);
     }
 
     [Fact]
-    public async Task Edit_Post_UpdatesTransactionAndCallsUpdateService()
+    public async Task Details_ReturnsPartialView()
     {
         // Arrange
         var (controller, txMock, catMock, _) =
             UnitTestsHelper.CreateTransactionControllerWithMocks();
         var transaction = UnitTestsHelper.GetTestTransaction();
-        var transactionVm = UnitTestsHelper.GetTransactionViewModel();
+        var category = UnitTestsHelper.GetTestCategory();
 
         txMock.Setup(s => s.GetByIdAsync(transaction.Id)).ReturnsAsync(transaction);
-        txMock.Setup(s => s.UpdateAsync(transaction.Id)).ReturnsAsync(transaction);
+        catMock.Setup(s => s.GetByIdAsync(transaction.CategoryId)).ReturnsAsync(category);
+
+        // Act
+        var result = await controller.Details(transaction.Id);
+
+        // Assert
+        var partialViewResult = Assert.IsType<PartialViewResult>(result);
+        Assert.Equal("_DetailsModalPartial", partialViewResult.ViewName);
+        var model = Assert.IsType<TransactionViewModel>(partialViewResult.Model);
+        Assert.Equal(transaction.Amount, model.Amount);
+    }
+
+    [Fact]
+    public async Task Create_Get_ReturnsPartialView()
+    {
+        // Arrange
+        var (controller, _, catMock, _) = UnitTestsHelper.CreateTransactionControllerWithMocks();
+        catMock.Setup(s => s.GetAllAsync()).ReturnsAsync(new List<Category>());
+
+        // Act
+        var result = await controller.Create();
+
+        // Assert
+        var partialViewResult = Assert.IsType<PartialViewResult>(result);
+        Assert.Equal("_CreateModalPartial", partialViewResult.ViewName);
+        Assert.IsType<TransactionViewModel>(partialViewResult.Model);
+    }
+
+    [Fact]
+    public async Task Create_Post_ValidModel_ReturnsCreated()
+    {
+        // Arrange
+        var (controller, txMock, _, _) = UnitTestsHelper.CreateTransactionControllerWithMocks();
+        var transactionVm = new TransactionViewModel { Name = "New Tx", Amount = 10m };
+        txMock.Setup(s => s.AddAsync(transactionVm)).ReturnsAsync(new Transaction());
+
+        // Act
+        var result = await controller.Create(transactionVm);
+
+        // Assert
+        Assert.IsType<CreatedResult>(result);
+        txMock.Verify(s => s.AddAsync(transactionVm), Times.Once);
+    }
+
+    [Fact]
+    public async Task Create_Post_Failure_ReturnsPartialView()
+    {
+        // Arrange
+        var (controller, txMock, catMock, _) =
+            UnitTestsHelper.CreateTransactionControllerWithMocks();
+        var transactionVm = new TransactionViewModel { Name = "New Tx", Amount = 10m };
+        txMock.Setup(s => s.AddAsync(transactionVm)).ReturnsAsync((Transaction)null);
+        catMock.Setup(s => s.GetAllAsync()).ReturnsAsync(new List<Category>());
+
+        // Act
+        var result = await controller.Create(transactionVm);
+
+        // Assert
+        var partialViewResult = Assert.IsType<PartialViewResult>(result);
+        Assert.Equal("_CreateModalPartial", partialViewResult.ViewName);
+        Assert.False(controller.ModelState.IsValid);
+    }
+
+    [Fact]
+    public async Task Edit_Get_ReturnsPartialView()
+    {
+        // Arrange
+        var (controller, txMock, catMock, _) =
+            UnitTestsHelper.CreateTransactionControllerWithMocks();
+        var transaction = UnitTestsHelper.GetTestTransaction();
+        txMock.Setup(s => s.GetByIdAsync(transaction.Id)).ReturnsAsync(transaction);
+        catMock.Setup(s => s.GetAllAsync()).ReturnsAsync(new List<Category>());
+
+        // Act
+        var result = await controller.Edit(transaction.Id);
+
+        // Assert
+        var partialViewResult = Assert.IsType<PartialViewResult>(result);
+        Assert.Equal("_EditModalPartial", partialViewResult.ViewName);
+        var model = Assert.IsType<TransactionViewModel>(partialViewResult.Model);
+        Assert.Equal(transaction.Id, model.TransactionId);
+    }
+
+    [Fact]
+    public async Task Edit_Post_UpdatesAndReturnsCreatedAtAction()
+    {
+        // Arrange
+        var (controller, txMock, _, _) = UnitTestsHelper.CreateTransactionControllerWithMocks();
+        var transaction = UnitTestsHelper.GetTestTransaction();
+        var transactionVm = new TransactionViewModel
+        {
+            TransactionId = transaction.Id,
+            Name = "Updated Tx",
+            Amount = 20m,
+            CategoryId = transaction.CategoryId,
+        };
+        txMock.Setup(s => s.GetByIdAsync(transaction.Id)).ReturnsAsync(transaction);
 
         // Act
         var result = await controller.Edit(transaction.Id, transactionVm);
 
         // Assert
-        var ok = Assert.IsType<CreatedAtActionResult>(result);
-        var returned = Assert.IsType<Transaction>(ok.Value);
-        Assert.Equal(transactionVm.Name, returned.Name);
-        Assert.Equal(transactionVm.Amount, returned.Amount);
-        Mock.Get(txMock.Object).Verify(s => s.UpdateAsync(transaction.Id), Times.Once);
+        var createdResult = Assert.IsType<CreatedAtActionResult>(result);
+        Assert.Equal("Details", createdResult.ActionName);
+        txMock.Verify(s => s.UpdateAsync(transaction.Id, transactionVm), Times.Once);
     }
 
     [Fact]
-    public async Task DeleteConfirmed_CallsDeleteAndRedirectsToIndex()
+    public async Task Delete_Get_ReturnsPartialView()
     {
+        // Arrange
         var (controller, txMock, catMock, _) =
             UnitTestsHelper.CreateTransactionControllerWithMocks();
+        var transaction = UnitTestsHelper.GetTestTransaction();
+        var category = UnitTestsHelper.GetTestCategory();
+        txMock.Setup(s => s.GetByIdAsync(transaction.Id)).ReturnsAsync(transaction);
+        catMock.Setup(s => s.GetByIdAsync(transaction.CategoryId)).ReturnsAsync(category);
 
-        txMock.Setup(s => s.DeleteAsync(9)).ReturnsAsync(DeleteResult.Success()).Verifiable();
+        // Act
+        var result = await controller.Delete(transaction.Id);
 
-        var result = await controller.DeleteConfirmed(9);
+        // Assert
+        var partialViewResult = Assert.IsType<PartialViewResult>(result);
+        Assert.Equal("_DeleteModalPartial", partialViewResult.ViewName);
+        var model = Assert.IsType<TransactionViewModel>(partialViewResult.Model);
+        Assert.Equal(transaction.Name, model.Name);
+    }
 
-        var redirect = Assert.IsType<RedirectToActionResult>(result);
-        Assert.Equal(nameof(TransactionController.Index), redirect.ActionName);
-        txMock.Verify(s => s.DeleteAsync(9), Times.Once);
+    [Fact]
+    public async Task DeleteConfirmed_Post_RedirectsToIndex()
+    {
+        // Arrange
+        var (controller, txMock, _, _) = UnitTestsHelper.CreateTransactionControllerWithMocks();
+        int transactionId = 1;
+
+        // Act
+        var result = await controller.DeleteConfirmed(transactionId);
+
+        // Assert
+        var redirectResult = Assert.IsType<RedirectToActionResult>(result);
+        Assert.Equal("Index", redirectResult.ActionName);
+        txMock.Verify(s => s.DeleteAsync(transactionId), Times.Once);
     }
 }
